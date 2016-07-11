ff95a30feada01f51543a18175f91f3f38f83179
Gabriela Rotver <gabrielarotver86@gmail.com>
2016-07-11 17:33:03 -0400

    Better version of cats in hats

--- catsInHats.rb
+++ catsInHats.rb
@@ -1,56 +1,31 @@
 cats = []
 
 # There are 100 cats in a row all wearing hats. 🐱
-100.times do 
-	cats << "😹" #cats.push("😹")
+100.times do
+    cats << "😹" #cats.push("😹")
 end
 
-# On the first pass, visit every cat and remove its hat. 🙀
-(0...100).each do |i|
-	cats[i] = "🙀"
+(1..100).each do |divisible|
+
+    (0..99).each do |i|
+        if((i+1) % divisible == 0)
+            if (cats[i] == "😹")
+                cats[i] = "🙀"
+            else
+                cats[i] = "😹"
+            end
+        end
+    end
 end
 
-# On the second pass, visit every other cat and replace its hat. 😺
-(0...100).each do |i|
-	if((i+1) % 2 == 0)
-		cats[i] = "😹"
-	end
+numbers = []
+puts "The positions (starting from 1) in the array that don't have hats are:"
+
+(0..99).each do |i|
+    if (cats[i] == "🙀")
+        numbers << (i+1)
+    end
 end
 
-
-# On the third pass, visit every third cat. 
-# If the cat is hatted, remove its hat 😿. 
-# If the cat is un-hatted, replace its hat. 😺
-(0...100).each do |i|
-	if ((i+1) % 3 == 0)
-		if (cats[i] == "😹")
-			cats[i] = "🙀"
-		else
-			cats[i] = "😹"
-		end
-	end
-end
-
-# On the 4th-100th passes, do the same.
-# Add hats if they don’t exist and remove hats if they do.
-(0...100).each do |i|
-	if (cats[i] == "😹")
-		cats[i] = "🙀"
-	else
-		cats[i] = "😹"
-	end
-end
-
-# On the 100th pass, you should only visit the 100th cat (the last cat),
-# removing or replacing its hat appropriately.
-(0...100).each do |i|
-	if ((i+1) % 100 == 0)
-		if (cats[i] == "😹")
-			cats[i] = "🙀"
-		else	
-			cats[i] = "😹"
-		end
-	end
-end
-
-puts cats
+p numbers
+#puts cats
